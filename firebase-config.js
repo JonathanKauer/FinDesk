3024850f775fa32fee31436611e013ec237a86cb
JonathanKauer <jonathan.kauer@guiainvest.com.br>
2025-02-19 19:05:21 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
+// firebase-config.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCGBrOC7ZCJ0y_MWM7EPxQRTw79BrXN5LA",
   authDomain: "findesktickets.firebaseapp.com",
@@ -13,5 +11,5 @@ const firebaseConfig = {
   appId: "1:652399228669:web:40871604bbb86ca5163094"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
