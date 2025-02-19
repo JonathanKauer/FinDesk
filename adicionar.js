6acce0dbcb2de40afb38b5476afe7bf427b1e920
JonathanKauer <jonathan.kauer@guiainvest.com.br>
2025-02-19 18:51:40 -0300

    Update adicionar.js

--- adicionar.js
+++ adicionar.js
@@ -1,2 +1,11 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "./firebase-config";
+
+async function adicionarRegistro(data) {
+  try {
+    const docRef = await addDoc(collection(db, "financas"), data);
+    console.log("Documento adicionado com ID:", docRef.id);
+  } catch (e) {
+    console.error("Erro ao adicionar documento:", e);
+  }
+}
