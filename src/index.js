cb7bbd77738035db22d31736bfef43c8f0c9ff3d
JonathanKauer <jonathan.kauer@guiainvest.com.br>
2025-02-19 19:18:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';  // Importa as diretivas do Tailwind e quaisquer outros estilos globais
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
