43862a50500f8b559929a99da6cd7525030d7f9c
JonathanKauer <jonathan.kauer@guiainvest.com.br>
2025-02-20 19:31:01 -0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123def456"
+const firebaseConfig = {
+  apiKey: "AIzaSyCGBrOC7ZCJ0y_MWM7EPxQRTw79BrXN5LA",
+  authDomain: "findesktickets.firebaseapp.com",
+  projectId: "findesktickets",
+  storageBucket: "findesktickets.firebasestorage.app",
+  messagingSenderId: "652399228669",
+  appId: "1:652399228669:web:40871604bbb86ca5163094"
 };
 
 const app = initializeApp(firebaseConfig);
