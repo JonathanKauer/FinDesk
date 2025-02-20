90791ef8ee7cb743e7e0895af8861fb6257a0b81
JonathanKauer <jonathan.kauer@guiainvest.com.br>
2025-02-20 19:09:14 -0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,19 @@
 // src/firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGBrOC7ZCJ0y_MWM7EPxQRTw79BrXN5LA",
-  authDomain: "findesktickets.firebaseapp.com",
-  projectId: "findesktickets",
-  storageBucket: "findesktickets.firebasestorage.app",
-  messagingSenderId: "652399228669",
-  appId: "1:652399228669:web:40871604bbb86ca5163094"
+  apiKey: "SUA_API_KEY",
+  authDomain: "seu-projeto.firebaseapp.com",
+  projectId: "seu-projeto",
+  storageBucket: "seu-projeto.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc123def456"
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Exporta o Firestore e o Auth para uso em outros arquivos
 export const db = getFirestore(app);
+export const auth = getAuth(app);
